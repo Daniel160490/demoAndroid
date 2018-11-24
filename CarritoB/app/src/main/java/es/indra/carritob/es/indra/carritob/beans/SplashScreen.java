38e5cc2440aba570ba6c946f6ac67d28604ef310
Daniel Garcia Diaz <daniel@Air-de-Daniel.home>
2018-11-24 20:18:35 +0100

    Añadida pantalla de carga en la App

--- CarritoB/app/src/main/java/es/indra/carritob/es/indra/carritob/beans/SplashScreen.java
+++ CarritoB/app/src/main/java/es/indra/carritob/es/indra/carritob/beans/SplashScreen.java
@@ -8,6 +8,7 @@ import android.widget.ProgressBar;
 import android.os.Bundle;
 import android.content.Intent;
 
+import es.indra.carritob.Login;
 import es.indra.carritob.MainActivity;
 import es.indra.carritob.R;
 
@@ -45,7 +46,7 @@ public class SplashScreen extends Activity{
                     i++;
                 }else{
                     timer.cancel();
-                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    Intent intent = new Intent(SplashScreen.this,Login.class);
                     startActivity(intent);
                     finish();
                 }
